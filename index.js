f15711494e64268cc9196d1dc0a65b8cc58ff25b
euphoriccheckers <98302222+euphoriccheckers@users.noreply.github.com>
2023-09-26 12:00:48 -0700

    Setup repo and restate problem

---
+++ index.js
@@ -0,0 +1,18 @@
+/* 
+constraints:
+    rock beats scissors
+    scissors beat paper
+    paper beats rock
+
+problem:
+    user plays against computer
+
+    (1)a function that returns random choices (rock, paper, or scissors)
+
+    (2)a function that plays a round of the game taking in 
+    playerSelection and computerSelection, 
+    returning a string that declares the winner of the round
+
+    (3)a function that plays 5 rounds of the game and keeps score
+    and reports a winner or loser at the end
+*/
