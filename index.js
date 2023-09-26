af579a0e6502cf927d635006c07cf0c0b0351e0c
euphoriccheckers <98302222+euphoriccheckers@users.noreply.github.com>
2023-09-26 12:40:38 -0700

    Complete playRound function

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ problem:
     (2)a function that plays a round of the game taking in 
     playerSelection and computerSelection, 
     returning a string that declares the winner of the round
+        --inputs must be case insensitive
 
     (3)a function that plays 5 rounds of the game and keeps score
     and reports a winner or loser at the end
@@ -33,5 +34,66 @@ function getComputerChoice () {
     else {
         //return paper
         return 'Paper';
+    }
+}
+
+//(2)
+function playRound (playerSelection, computerSelection) {
+    //take in playerSelection and computerSelection
+    //convert both to upper case
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+    //compare playerSelection and computerSelection
+    switch (playerSelection) {
+        case 'ROCK': {
+            switch (computerSelection) {
+                case 'ROCK': {
+                    return 'NOT SURE WHAT TO DO HERE';
+                    break;
+                }
+                case 'PAPER': {
+                    return 'You lose! Paper beats Rock';
+                    break;
+                }
+                case 'SCISSORS': {
+                    return 'You win! Rock beats Scissors';
+                    break;
+                }
+            }
+            break;
+        }
+        case 'PAPER': {
+            switch (computerSelection) {
+                case 'ROCK': {
+                    return 'You win! Paper beats Rock';
+                    break;
+                }
+                case 'PAPER': {
+                    return 'NOT SURE WHAT TO DO HERE';
+                    break;
+                }
+                case 'SCISSORS': {
+                    return 'You lose! Scissors beat Paper';
+                    break;
+                }
+            }
+            break;
+        }
+        case 'SCISSORS': {
+            switch (computerSelection) {
+                case 'ROCK': {
+                    return 'You lose! Rock beats Scissors';
+                    break;
+                }
+                case 'PAPER': {
+                    return 'You win! Scissors beat Paper';
+                    break;
+                }
+                case 'SCISSORS': {
+                    return 'NOT SURE WHAT TO DO HERE';
+                    break;
+                }
+            }
+        }
     }
 }
